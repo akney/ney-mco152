8a0c7cc887e606862eb30d32b034ad9b1e9caea4
Katie Ney <akkn1995@gmail.com>
2015-12-09 22:44:28 -0500

    Hw update

--- src/ney/weather/WeatherGui.java
+++ src/ney/weather/WeatherGui.java
@@ -1,8 +1,11 @@
 package ney.weather;
 
+import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.IOException;
 
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
@@ -12,10 +15,13 @@ import javax.swing.JTextField;
 
 public class WeatherGui extends JFrame {
 	private JPanel top;
+	private JPanel middle;
 	private JPanel bottom;
 	private JLabel zip;
 	private JTextField zipEntry;
+	private JLabel notZipcode;
 	private JLabel temp;
+	private JLabel tempNum;
 	private JLabel description;
 
 	public WeatherGui() {
@@ -23,8 +29,23 @@ public class WeatherGui extends JFrame {
 		setSize(800, 600);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+		setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+
 		top = new JPanel();
+		top.setBackground(Color.BLUE);
 		top.setLayout(new BoxLayout(top, BoxLayout.LINE_AXIS));
+
+		middle = new JPanel();
+		middle.setBackground(Color.BLUE);
+		middle.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 40));
+
+		bottom = new JPanel();
+		bottom.setBackground(Color.BLUE);
+		bottom.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 40));
+
+		temp = new JLabel("Tempature");
+		temp.setFont(new Font("Courier New", Font.BOLD, 28));
+		middle.add(temp);
 
 		zip = new JLabel("Zipcode: ");
 		zip.setAlignmentX(CENTER_ALIGNMENT);
@@ -50,13 +71,21 @@ public class WeatherGui extends JFrame {
 
 			public void all(KeyEvent e) {
 				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-					ConnectToInternet cti = new ConnectToInternet(zipEntry.getText());
+					try {
+						ConnectToInternet cti = new ConnectToInternet(zipEntry.getText());
+
+					} catch (IOException e1) {
+						notZipcode = new JLabel("Not a valid zipcode.");
+						notZipcode.setFont(new Font("Courier New", Font.BOLD, 28));
+						top.add(notZipcode);
+					}
 				}
 			}
 		});
 		top.add(zip);
 		top.add(zipEntry);
 		add(top);
+		add(middle);
 
 	}
 
