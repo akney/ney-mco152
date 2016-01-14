3503c7344b37370b33dff5f350a1926e0ce02f77
Katie Ney <akkn1995@gmail.com>
2016-01-14 09:21:20 -0500

    Twitter

--- src/ney/twitter/TwitterHandler.java
+++ src/ney/twitter/TwitterHandler.java
@@ -15,7 +15,7 @@ public class TwitterHandler extends AbstractHandler {
 	public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException {
 		response.setContentType("text/html;charset=utf-8");
-		response.setStatus(HttpServletResponse.SC_OK);
+		response.setStatus(HttpServletResponse.SC_OK); // Sc_ is the method
 		baseRequest.setHandled(true);
 		response.getWriter().println(
 				"<h1>Hello World</h1><h3>Wow it worked!</h3><h3>Hodu LaHashem kee tov! Maybe I actually will pass this class!</h3>");
